a5f8c78eb30cb107186f40e975dac7cd0b3c675a
AuSoleil626 <2230004148@student.must.edu.mo>
2024-06-14 11:43:01 +0800

    完善Enemy类

--- ISAAC_Circle/Enemy.cpp
+++ ISAAC_Circle/Enemy.cpp
@@ -1,8 +1,35 @@
 #include "Enemy.h"
+#include <cmath>
+enemy_base::enemy_base(): move_direction_(sf::Glsl::Vec2(0, 0)), speed(0),location_(sf::Glsl::Vec2(0, 0))
+{
+}
+
+
+void enemy_base::set_move_speed(float move_speed)
+{
+    speed=move_speed;
+}
+
+float enemy_base::get_move_speed() const
+{
+    return speed;
+}
 
 sf::Glsl::Vec2 enemy_base::get_move_direction() const
 {
     return move_direction_;
+}
+
+sf::Glsl::Vec2 enemy_base::get_current_location() const
+{
+    return location_;
+}
+
+void enemy_base::move()
+{
+    float angle = std::atan2(move_direction_.y, move_direction_.x);
+    sf::Glsl::Vec2 temp_vec2(location_.x+speed*cos(angle),location_.y+speed*sin(angle));
+    location_=temp_vec2;
 }
 
 void enemy_base::set_move_direction(const sf::Glsl::Vec2& vec2)
--- ISAAC_Circle/Enemy.h
+++ ISAAC_Circle/Enemy.h
@@ -1,14 +1,32 @@
 #pragma once
 #include <SFML/Graphics/Glsl.hpp>
-
+constexpr float PI = 3.14159265358979323846f;
 class enemy_base
 {
 public:
+    enemy_base();
     virtual ~enemy_base()=default;
 
+    
+    void set_move_speed(float move_speed);
+    float get_move_speed() const;
     sf::Glsl::Vec2 get_move_direction() const;
+    sf::Glsl::Vec2 get_current_location() const;
+
+    /**
+     * @brief enemy moveFunction
+     * 
+     * According to the move_direction to calculate the next position
+     */
+    void move();
+
+    
     void set_move_direction(const sf::Glsl::Vec2& vec2);
 private:
     //Control the Enemy Move Direction
-    sf::Glsl::Vec2 move_direction_; 
+    sf::Glsl::Vec2 move_direction_;
+    //move speed
+    float speed;
+    //enemy location
+    sf::Glsl::Vec2 location_;
 };
